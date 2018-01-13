5ed9605b9ede1e2e799f2fbeab8f4d8cca287566
nguyenquocvietbmt1996@gmail.com <nguyenquocvietbmt1996@gmail.com>
2018-01-14 01:26:08 +0700

    use git

---
+++ Git.cpp
@@ -0,0 +1,30 @@
+
+// dang nhap
+
+git config --global user.email "nguyenquocvietbmt1996@gmail.com"
+
+//khoi dong Git
+
+git init
+
+//kiem tra file da untracked hay chua
+
+git status      // neu do thi chua tracked
+
+// tracked file
+
+git add "<ten file>"    // 1 file
+
+git add *             // tat ca cac file
+
+// commit
+
+git commit -m <noi dung>
+
+// tao reponsitory tren github sau do lay link ket noi voi git
+
+git remote add <ten> <http:url>
+
+// dua file hay thu muc len github
+
+git push <ten> <nhanh>  // thuong la nhanh lay master
