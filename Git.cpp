e80dd2fe4d3d43e1df3d227f7bdf04966177ea13
nguyenquocvietbmt1996@gmail.com <nguyenquocvietbmt1996@gmail.com>
2018-01-14 01:45:01 +0700

    commit

--- Git.cpp
+++ Git.cpp
@@ -13,13 +13,13 @@ git status      // neu do thi chua tracked
 
 // tracked file
 
-git add "<ten file>"    // 1 file
+git add <ten file>    // 1 file
 
 git add *             // tat ca cac file
 
 // commit
 
-git commit -m <noi dung>
+git commit -m "<noi dung>"
 
 // tao reponsitory tren github sau do lay link ket noi voi git
 
